314e4878fd597afb78a814d5205555af04278fd4
David <davidfnatt2002@gmail.com>
2026-07-09 13:43:07 +0200

    debug: print APP_KEY prefix in startup log

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,9 @@ set -e
 
 cd /var/www/html
 
+# Debug: print first 10 chars of APP_KEY to confirm it's loaded
+echo "APP_KEY prefix: $(echo "$APP_KEY" | cut -c1-10)"
+
 # Clear any cached config to ensure env vars are read fresh
 php artisan config:clear
 php artisan cache:clear
