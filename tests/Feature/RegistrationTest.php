66626fa486d65e30479cc70d984db4bd7d9faffd
David <davidfnatt2002@gmail.com>
2026-07-16 13:29:41 +0200

    fix: correct test redirects to /welcome and add RefreshDatabase to ExampleTest

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -40,14 +40,14 @@ class RegistrationTest extends TestCase
         }
 
         $response = $this->post('/register', [
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'password',
+            'name'                  => 'Test User',
+            'email'                 => 'test@example.com',
+            'password'              => 'password',
             'password_confirmation' => 'password',
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature(),
+            'terms'                 => Jetstream::hasTermsAndPrivacyPolicyFeature(),
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(route('dashboard', absolute: false));
+        $response->assertRedirect('/welcome');
     }
 }
